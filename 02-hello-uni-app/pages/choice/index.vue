40a729a5b136f6e778ea8099c3838c39b06c9975
zzc <zzc121382000@163.com>
2022-09-01 22:18:29 +0800

    22-9-1 基础操作

---
+++ 02-hello-uni-app/pages/choice/index.vue
@@ -0,0 +1,40 @@
+<template>
+	 <view>
+		 <!-- #ifdef H5 -->
+		 <view>
+			这是H5界面展示的东西
+		 </view>>
+		 <!-- #endif -->
+		 <!-- #ifdef MP-WEIXIN -->
+		 <view>
+			 这是微信小程序界面展示的东西
+		 </view>
+		 <!-- #endif -->
+		 <button @click="test" type="primary">test</button>
+	 </view>
+	
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			test(e){
+				console.log(e);
+				// #ifdef H5
+				console.log("这是H5界面专门的输出");
+				// #endif
+				// #ifdef MP-WEIXIN
+				console.log("这是微信小程序的界面");
+				// #endif
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ 02-hello-uni-app/pages/message/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<input placeholder="第一个数" v-model="data1"/>
+		<view>
+			this is a fgx
+		</view>
+		<input placeholder="第二个数" v-model="data2"/>
+		<button @click="test ">battle</button>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				data1:" ",
+				data2:" "
+			}
+		},
+		methods: {
+			test(){
+				this.data1>this.data2?wx.showToast({
+					title:"第一个数大"
+				}):wx.showToast({
+					title:"第二个数大"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	input{
+		background-color: bisque;
+	}
+</style>
